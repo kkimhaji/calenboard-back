78d9b1578a788e99e64a644afdd14def46993a05
하지현 <kkim.haji@gmail.com>
2022-06-09 21:06:23 +0900

    회원가입 css, html 수정

--- src/main/java/jejunu/portal/calenboard/controller/SignController.java
+++ src/main/java/jejunu/portal/calenboard/controller/SignController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/user")
 @RequiredArgsConstructor
 public class SignController {
 
