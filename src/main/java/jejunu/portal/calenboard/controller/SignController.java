c438b5358ce86b69ddbafac5c74812ed94c3f393
하지현 <kkim.haji@gmail.com>
2022-06-15 11:33:34 +0900

    jwt 토큰 만료여부 검사

--- src/main/java/jejunu/portal/calenboard/controller/SignController.java
+++ src/main/java/jejunu/portal/calenboard/controller/SignController.java
@@ -3,6 +3,7 @@ package jejunu.portal.calenboard.controller;
 import jejunu.portal.calenboard.entity.Member;
 import jejunu.portal.calenboard.model.SingleResult;
 import jejunu.portal.calenboard.repository.MemberRepository;
+import jejunu.portal.calenboard.security.JwtAuthenticationFilter;
 import jejunu.portal.calenboard.security.JwtTokenProvider;
 import jejunu.portal.calenboard.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.FailedLoginException;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -23,6 +25,7 @@ public class SignController {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @PostMapping("/signin")
     public SingleResult<String> signin(@RequestParam String email, @RequestParam String password) throws FailedLoginException {
@@ -32,6 +35,12 @@ public class SignController {
     @PostMapping("/signup")
     public Member signup(@RequestParam String email, @RequestParam String password, @RequestParam String nickname) throws Exception {
         return memberService.signup(email, password, nickname);
+    }
+
+    @PostMapping("/token")
+    public boolean checkToken(HttpServletRequest request){
+        String token = jwtTokenProvider.resolveToken(request);
+        return jwtTokenProvider.validateToken(token);
     }
 
 
